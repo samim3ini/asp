f09f5505db9714a46b3a5b4549c52b935da93692
samim3ini <sjm111cia@gmail.com>
2025-04-07 04:07:07 +0300

    auth changed

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Authenticator } from '@aws-amplify/ui-react';
 import App from "./App.tsx";
-import theme from './theme';
-import { CssBaseline, ThemeProvider, Box, Typography, Paper } from '@mui/material';
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css';
@@ -12,51 +10,8 @@ Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Authenticator hideSignUp
-       components={{
-        Header() {
-          return (
-            <Paper
-              elevation={3}
-              sx={{
-                textAlign: 'center',
-                marginTop: 5,
-                marginBottom: 3,
-                padding: 3,
-                backgroundColor: 'primary.main',
-                color: 'white',
-                borderRadius: 2,
-              }}
-            >
-              <Typography variant="h5" component="h1" sx={{ fontWeight: 'bold' }}>
-                Attendance System Admin Portal
-              </Typography>
-            </Paper>
-          );
-        },
-        Footer() {
-          return (
-            <Box
-              sx={{
-                textAlign: 'center',
-                marginTop: 3,
-                padding: 2,
-                color: 'text.secondary',
-                fontSize: '0.9rem',
-              }}
-            >
-              <Typography variant="body2">
-                © {new Date().getFullYear()} Attendnace System Admin Portal. All rights reserved.
-              </Typography>
-            </Box>
-          );
-        },
-      }}
-    >
+    <Authenticator hideSignUp >
       <App />
     </Authenticator>
-  </ThemeProvider>
   </React.StrictMode >
 );
